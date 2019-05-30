580c1e44a6e966db098ac049b82d78a33f9b94df
kolyanjy <kolyan654d@gmail.com>
2019-05-30 18:18:06 +0300

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.generators do |g|
-  g.test_framework :rspec
-end
-
 module Bookstore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      g.test_framework :rspec
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
