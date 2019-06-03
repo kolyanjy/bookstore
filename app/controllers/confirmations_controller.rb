c81d1f99d8ed13b27b335502c74c37be78d0998c
kolyanjy <kolyan654d@gmail.com>
2019-06-03 17:23:35 +0300

    fix rails_best_practics

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,8 +1,2 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  private
-
-  def after_confirmation_path_for(_resource_name, resource)
-    sign_up(resource)
-    your_new_after_confirmation_path
-  end
 end
