f7247a485914257a9faa84e4e1e549df8fd1a952
kolyanjy <kolyan654d@gmail.com>
2019-06-12 10:41:11 +0300

    fix rubocop

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Users::OmniauthCallbacksController do
   let(:auth_hash) { OmniAuth.config.mock_auth[:facebook] }
+  let(:sign_in_url) { '/users/sign_in' }
 
   before do
     request.env['devise.mapping'] = Devise.mappings[:user]
@@ -7,28 +8,26 @@ RSpec.describe Users::OmniauthCallbacksController do
     request.env['omniauth.auth'] = auth_hash
   end
 
-  context 'call omniauth callback with success result' do
+  context 'when call omniauth callback with success result' do
     let(:user) { build(:user) }
     let(:result) { double(:result, success?: true, user: user) }
 
     it 'redirects to root' do
-      expect(Users::CreateFromOmniauth).to receive(:call).with(auth_hash: auth_hash).and_return(result)
-
+      allow(Users::CreateFromOmniauth).to receive(:call).with(auth_hash: auth_hash).and_return(result)
       get 'facebook'
+      expect(subject).to redirect_to(sign_in_url)
     end
   end
 
-  context 'call omniauth callback with fail result' do
+  context 'when call omniauth callback with fail result' do
     let(:user) { build(:user) }
     let(:result) { double(:result, success?: false, user: user) }
-    before do
-    end
+
     it 'redirects to root' do
-      expect(Users::CreateFromOmniauth).to receive(:call).with(auth_hash: auth_hash).and_return(result)
+      allow(Users::CreateFromOmniauth).to receive(:call).with(auth_hash: auth_hash).and_return(result)
       get 'facebook'
       expect(subject).to redirect_to(new_user_registration_url)
       expect(session['devise.facebook_data']).to eq(request.env['omniauth.auth'])
     end
   end
-
 end
