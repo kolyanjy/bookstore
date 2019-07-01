af1c41416087e51e408dca2d1dc5dec73b0e0e73
kolyanjy <kolyan654d@gmail.com>
2019-07-01 12:59:05 +0300

    add long dascription functionality

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function () {
+  $('#btn_read_more').click(function () {
+    $('#small_text').hide();
+    $('#block_more').css('display', 'inline');
+    $('#btn_read_more').hide();
+  });
+
+  $('#btn_hide_read_more').click(function () {
+    $('#small_text').show();
+    $('#block_more').hide();
+    $('#btn_read_more').show();
+  })
+});
