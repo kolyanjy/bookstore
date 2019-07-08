20d53e6d5419779445b1eeb7181b5d3d3ce780d4
kolyanjy <kolyan654d@gmail.com>
2019-07-09 02:59:49 +0300

    change controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   decorates_assigned :slider_books
 
   def index
-    @best_seller = BookDecorator.decorate_collection(Book.first(4))
+    @best_seller = BookDecorator.decorate_collection(Book.first(NUMBER_BEST_SELLER))
     @slider_books = BookDecorator.decorate_collection(Book.last(NUMBER_SLIDER))
   end
 end
