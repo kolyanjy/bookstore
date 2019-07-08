91f7a4e9efe055a7df5997baac93187d045190ef
kolyanjy <kolyan654d@gmail.com>
2019-07-09 02:56:47 +0300

    add home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index; end
+  NUMBER_SLIDER = 3
+  NUMBER_BEST_SELLER = 4
+
+  decorates_assigned :slider_books
+
+  def index
+    @best_seller = BookDecorator.decorate_collection(Book.first(4))
+    @slider_books = BookDecorator.decorate_collection(Book.last(NUMBER_SLIDER))
+  end
 end
---
+++ spec/features/home/index_page_spec.rb
@@ -0,0 +1,43 @@
+RSpec.describe 'Index page', type: :feature do
+  let(:category) { create(:category) }
+  let!(:book) { create(:book, category: category) }
+  let(:categories) { create_list(:category, 3, :with_books) }
+
+  it 'click All books in header' do
+    visit root_path
+    find('li', class: 'dropdown', text: I18n.t('header.shop')).click
+    click_link('All books')
+    expect(page).to have_selector 'h1', text: I18n.t('word.catalog')
+    expect(page).to have_current_path books_path
+  end
+
+  it 'click slider book' do
+    category
+    visit root_path
+    find('div', class: 'general-img-wrap').find('a').click
+    expect(page).to have_current_path book_path(book.id)
+  end
+
+  it 'click All books in home page' do
+    visit root_path
+    find('a', class: 'navbar-brand', text: I18n.t('word.bookstore')).click
+    expect(page).to have_selector 'h2', text: I18n.t('home.welcome')
+  end
+
+  it 'click shop in header' do
+    category
+    visit root_path
+    find('li', class: 'dropdown', text: I18n.t('header.shop')).click
+    find('a', class: 'filter-link', text: category.name).click
+    expect(page).to have_selector 'h1', text: I18n.t('word.catalog')
+  end
+
+  it 'click button Get started' do
+    categories
+    visit root_path
+    find('a', class: 'btn-default', text: I18n.t('home.started')).click
+    categories.each do |category|
+      expect(page).to have_content category.name
+    end
+  end
+end
