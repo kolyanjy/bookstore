fd9a30bc75b1be9080f21b3025ba54319009cb11
kolyanjy <kolyan654d@gmail.com>
2019-07-10 19:32:29 +0300

    fix js

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,5 +1,6 @@
-$(document).on('turbolinks:load', function() {
+$(document).on("turbolinks:load", function() {
   $("#checkbox_consent").click(function() {
-    $("#checkbox_consent").prop( "checked" ) ? $("#button_delete_account").removeClass("disabled") : $("#button_delete_account").addClass("disabled");
+    $("#checkbox_consent").prop("checked") ? $("#button_delete_account")
+    .removeClass("disabled") : $("#button_delete_account").addClass("disabled");
   });
 });
