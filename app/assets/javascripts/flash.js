5721694a0a6de54d5c86fcbb0a40d6450621c982
kolyanjy <kolyan654d@gmail.com>
2019-07-11 17:46:10 +0300

    fix ci

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-$(document).on("turbolinks:load", function() {
-  $(function() {
-    setTimeout(function() {
-       $(".alert").slideUp(800)
-=======
 $(document).on('turbolinks:load', function() {
   $(function() {
     setTimeout(function() {
       $('.alert').slideUp(800)
->>>>>>> develop
      }, 15000);
   });
 });
