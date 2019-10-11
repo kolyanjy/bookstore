40c19228cda736ad074817bee2614b749ba3f254
Nikolai Kolesnikov <kolyan654d@gmail.com>
2019-10-11 18:37:01 +0300

    Check ci (#21)

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :book do
     name { Faker::Book.title }
-    price { Faker::Number.decimal }
-    description { Faker::Lorem.paragraph_by_chars }
-    date_of_publication { Faker::Date.birthday }
+    price { Faker::Number.positive(1.00, 1000.00) }
+    description { Faker::Lorem.paragraph_by_chars(256, false) }
+    date_of_publication { Faker::Date.birthday(10, 20) }
     height { rand(200..300) }
     width { rand(50..200) }
     depth { rand(10..50) }
