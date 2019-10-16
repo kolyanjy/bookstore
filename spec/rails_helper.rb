b1169d54482aa492a3721e5bb60ba047271fad33
kolyanjy <kolyan654d@gmail.com>
2019-10-16 14:23:44 +0300

    change config for specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,18 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_filter '/app/admin/'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
+require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,3 @@
-require 'rails_helper'
-require 'simplecov'
-
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/config/'
-  add_filter '/vendor/'
-  add_filter '/app/admin/'
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
